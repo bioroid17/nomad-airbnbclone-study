962cc8d247c9bb6ad77afe1e9ba38acf996ab75a
bioroid17 <trace1997@ajou.ac.kr>
2024-10-02 11:46:51 +0900

    Day 10: Urls, Views and Templates
    '/' url shows all Tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Tweet
+
 
 # Create your views here.
+def get_all_tweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "all_tweets.html",
+        {
+            "title": "Title from django",
+            "tweets": tweets,
+        },
+    )
